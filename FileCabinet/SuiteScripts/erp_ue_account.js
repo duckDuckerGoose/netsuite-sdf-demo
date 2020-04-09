fc20321df84c80623d16a46028c68a7aeb70454c
merik-ducker <merik.ducker@gmail.com>
2020-04-09 19:13:18 -0400

    added comments

--- FileCabinet/SuiteScripts/erp_ue_account.js
+++ FileCabinet/SuiteScripts/erp_ue_account.js
@@ -9,9 +9,13 @@ function(record) {
 
     function afterSubmit(context) {
         var account = context.newRecord;
+        // get value of 'DO NOT SET EXTERNAL ID TO NUMBER' checkbox
         var accountConfig = account.getValue('custrecord_erp_dont_set_external_auto');
+        // if the checkbox is not checked, proceed with setting the external id
         if (!accountConfig) {
+            // get the account number
             var accountNumber = account.getValue('acctnumber');
+            // set the external id using the account number
             record.submitFields({
                 type: account.type,
                 id: account.id,
